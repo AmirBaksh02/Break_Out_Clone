a169168e7e83bde1829dee6f16548374b62e179c
GalaxyBlitz <akbaksh02@hotmail.com>
2017-09-04 18:49:10 +0100

    Create DeadZone.cs

---
+++ DeadZone.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class DeadZone : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider col)
+    {
+        GM.instance.LoseLife(); 
+    }
+}
+
